abe695f6c13beee142e9c641f2f27eb305902c64
Homework2 <ivliev.k@yandex.ru>
2023-01-23 17:58:47 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
         int[] arr1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         int[] arr2 = new int[10];
+        System.out.println(Arrays.toString(arr1));
         for (int i = 0; i < arr1.length; i++) {
             arr2[i] = arr1[arr1.length - 1 - i];
 
