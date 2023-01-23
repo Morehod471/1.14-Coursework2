ec0182bccfb37a2e72d5881db498e2c1a72cc13c
Homework2 <ivliev.k@yandex.ru>
2023-01-23 17:12:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] arr2 = new int[10];
+        for (int i = 0; i < arr1.length; i++) {
+            arr2[i] = arr1[arr1.length - 1 - i];
+
+        }
+        System.out.println(Arrays.toString(arr2));
+    }
+}
