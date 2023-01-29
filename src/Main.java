3de3ac3e0f1dc311af2c22e1e1960440652503c0
Homework2 <ivliev.k@yandex.ru>
2023-01-29 10:55:22 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
         int[] arr1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         int[] arr2 = new int[10];
         System.out.println(Arrays.toString(arr1));
